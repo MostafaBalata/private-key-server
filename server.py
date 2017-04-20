ad9b543201d773633038937d6879eb7defd9630e
Leonid Logvinov <logvinov.leon@gmail.com>
2017-04-20 15:13:32 +0200

    Use uuid from jwt

--- server.py
+++ server.py
@@ -1,16 +1,17 @@
 import os
 
-from bip32utils import BIP32Key
-from ecdsa import BadSignatureError
-from ethereum.utils import sha3
+import bitcoin as b
+from uuid import UUID
+from ethereum.utils import int_to_big_endian, privtopub, sha3
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 import auth
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
-
 app.config.from_mapping(os.environ)
+CORS(app)
 
 for key in app.config["REQUIRED_ENV_CONFIG_FIELDS"]:
     if key not in os.environ:
@@ -19,38 +20,40 @@ for key in app.config["REQUIRED_ENV_CONFIG_FIELDS"]:
 master_entropy = bytes.fromhex(app.config["MASTER_ENTROPY"])
 
 
-def pub_to_addr(pub):
-    assert (len(pub) == 64)
-    return sha3(pub)[12:]
+def _user_private_key(uuid):
+    user_entropy = UUID(uuid).bytes
+    return sha3(b'%b%b' % (user_entropy, master_entropy))[:32]
 
 
-@app.route('/api/<int:uid>/sign', methods=['POST'])
+def _user_public_key(uuid):
+    user_private_key = _user_private_key(uuid)
+    return privtopub(user_private_key)
+
+
+def _user_address(uuid):
+    public_key = _user_public_key(uuid)
+    return "0x" + sha3(public_key[1:])[12:].hex()
+
+
+@app.route('/api/address', methods=['GET'])
 @auth.verify_jwt(check=auth.verify_logged_in)
-def sign_message_by_user(uid):
-    user_entropy = sha3(b'%d%b' % (uid, master_entropy))
-    user_private_key = BIP32Key.fromEntropy(user_entropy)
-    message = request.get_json()["message"]
-    binary_message = message.encode('utf-8')
-    signature = user_private_key.k.sign(binary_message).hex()
+def get_address():
+    return _user_address(request.authorization["uuid"])
+
+
+@app.route('/api/sign', methods=['POST'])
+@auth.verify_jwt(check=auth.verify_logged_in)
+def sign_message_by_user():
+    user_private_key = _user_private_key(request.authorization["uuid"])
+    message = bytes.fromhex(request.get_json()["message"])
+    digest = sha3(message)
+    v, r, s = b.ecdsa_raw_sign(digest, user_private_key)
     return jsonify({
-        "signature": signature,
-        "message": message
+        "v": int_to_big_endian(v).hex(),
+        "r": int_to_big_endian(r).hex(),
+        "s": int_to_big_endian(s).hex(),
+        "message": request.get_json()["message"]
     })
-
-
-@app.route('/api/<int:uid>/verify', methods=['POST'])
-def verify_signed_message(uid):
-    user_entropy = sha3(b'%d%b' % (uid, master_entropy))
-    user_private_key = BIP32Key.fromEntropy(user_entropy)
-    data = request.get_json()
-    verifying_key = user_private_key.K
-    signature = bytes.fromhex(data["signature"])
-    message = data["message"].encode('utf-8')
-    try:
-        verifying_key.verify(signature, message)
-        return jsonify({"ok": True})
-    except BadSignatureError:
-        return jsonify({"ok": False})
 
 
 @app.errorhandler(403)
